55eaff2c0a29a109fae5c8274a414340de36d005
Cong Luwen <congve1@live.com>
2017-05-09 17:16:28 +0800

    finish the segment_graph method

--- segmentGraph.py
+++ segmentGraph.py
@@ -1,5 +1,5 @@
 import disjointSet
-class edge:
+class Edge:
     def __init__(self,w=0,a=0,b=0):
         self.w = w
         self.a = a
@@ -11,5 +11,26 @@ def threshold(size,c):
 
 """
 Segment a graph
-Returns a dis
+Returns a disjoint-set forest representing the segmentation
+nu_vertices:number of vertices in the graph
+nu_edges: number of edges in the graph
+c: constant for threshold function
 """
+def  segment_graph(nu_vertices,nu_edges,edges,c):
+    tmp = edges[:nu_edges]
+    tmp.sort()
+    edges[:nu_edges] = tmp
+    u = disjointSet.Universe(nu_vertices)
+    thresholds = [threshold(1,c) for i in range(nu_vertices)]
+    loop_range= range(nu_edges)
+    for i in loop_range:
+        edge = edges[i]
+        a = u.find(edge.a)
+        b = u.find(edge.b)
+        if a != b:
+            if edge.w <= thresholds[a] and \
+                edge.w <= thresholds[b]:
+                u.join(a,b)
+                a = u.find(a)
+                thresholds[a] = edge.w + threshold(u.size(a),c)
+    return u
